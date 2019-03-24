9308bce9abe40e6dedbdd23e722e6d8276d6344d
Daniel <daniel.digirolamo5@gmail.com>
2019-03-24 12:33:31 +0100

    svolto esercizio lez 9

--- app/src/main/java/com/insegno/luca/weatherapp/MainActivity.java
+++ app/src/main/java/com/insegno/luca/weatherapp/MainActivity.java
@@ -18,6 +18,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.NumberFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     public void searchCity(View view){
         String city = ((EditText)findViewById(R.id.cityInput)).getText().toString();
         try {
-            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=");
+            URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=bac270db3285a9dd02441776a6d08870");
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
             BufferedReader buff = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
@@ -45,6 +46,13 @@ public class MainActivity extends AppCompatActivity {
             double tempK = json.getJSONObject("main").getDouble("temp");
             double tempC = tempK - 273.15;
             String sky = json.getJSONArray("weather").getJSONObject(0).getString("main");
+            int pressione = json.getJSONObject("main").getInt("pressure");
+            int umidità = json.getJSONObject("main").getInt("humidity");
+            double tempMinK = json.getJSONObject("main").getDouble("temp_min");
+            double tempMaxK = json.getJSONObject("main").getDouble("temp_max");
+            double tempMinC = tempMinK - 273.15;
+            double tempMaxC = tempMaxK - 273.15;
+
             //Log di controllo
             Log.i("WEATHER", "Latitudine:" + latitudine + ", Longitudine:" + longitudine);
             Log.i("WEATHER", "temperatura" + tempC);
@@ -56,6 +64,10 @@ public class MainActivity extends AppCompatActivity {
             ((TextView)findViewById(R.id.sky)).setText(sky);
             ((TextView)findViewById(R.id.coords)).setText(latitudine + ", " + longitudine);
             ((TextView)findViewById(R.id.temp)).setText(String.valueOf(formatter.format(tempC) + " °C"));
+            ((TextView)findViewById(R.id.pressure)).setText(pressione + " mbar");
+            ((TextView)findViewById(R.id.humidity)).setText(umidità + "%");
+            ((TextView)findViewById(R.id.tempMin)).setText(String.valueOf(formatter.format(tempMinC) + " °C"));
+            ((TextView)findViewById(R.id.tempMax)).setText(String.valueOf(formatter.format(tempMaxC) + " °C"));
 
             ((EditText)findViewById(R.id.cityInput)).setText("");
 
